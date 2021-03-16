f002ff870bd33229c270ea22956aa511af4bb0e9
thebeardless <jakepeterwebster@gmail.com>
2021-03-16 22:16:06 +1000

    started tut

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import { Button } from "@material-ui/core";
+import React from "react";
+import { Grid } from "@material-ui/core";
+import Header from "./Header";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <Button color="primary" variant="outlined">
-        This is a button
-      </Button>
-    </div>
+    <Grid container direction="column">
+      <Grid item>
+        <Header></Header>
+      </Grid>
+      <Grid item container>
+        <Grid item xs={0} sm={2}></Grid>
+        <Grid item xs={12} sm={8}>
+          Content
+        </Grid>
+        <Grid item xs={0} sm={2}></Grid>
+      </Grid>
+      <Grid item></Grid>
+    </Grid>
   );
-}
+};
 
 export default App;
